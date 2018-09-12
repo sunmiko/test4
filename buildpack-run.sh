57185bc17f92136dc400d580f8aab2923768d381
Michael Fang <mfang@salesforce.com>
2018-09-12 15:54:42 -0700

    test git clone

--- buildpack-run.sh
+++ buildpack-run.sh
@@ -2,4 +2,4 @@
 #shunit2-2.1.6/src/shunit2 tests/test_wordcount.sh
 
 echo "Testing git clone"
-git clone https://github.com/Localization/label-service.git
+git clone https://svc-l10n:Globalization_123456@github.com/Localization/label-service.git
