b8f8f822ff035c800227ad182d4a8d971e496e3e
Michael Fang <mfang@salesforce.com>
2018-09-12 15:52:14 -0700

    test git clone

--- buildpack-run.sh
+++ buildpack-run.sh
@@ -1,2 +1,5 @@
-echo "Testing using heroku-buildpack-run"
-shunit2-2.1.6/src/shunit2 tests/test_wordcount.sh
+#echo "Testing using heroku-buildpack-run"
+#shunit2-2.1.6/src/shunit2 tests/test_wordcount.sh
+
+echo "Testing git clone"
+git clone https://github.com/Localization/label-service.git
