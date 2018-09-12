ff8093c08d193b93ae7f140f1ed2091a1d910154
sunmiko <30559933+sunmiko@users.noreply.github.com>
2018-09-12 16:16:07 -0700

    Update buildpack-run.sh

--- buildpack-run.sh
+++ buildpack-run.sh
@@ -2,4 +2,5 @@
 #shunit2-2.1.6/src/shunit2 tests/test_wordcount.sh
 
 echo "Testing git clone"
+rm -rf label-service
 git clone https://svc-l10n:Globalization_123456@github.com/Localization/label-service.git
