87d42b641900a68ffc95f18196396e4b4373806a
Michael Fang <mfang@salesforce.com>
2018-09-12 15:01:22 -0700

    updating test_wordcount.sh

--- tests/test_wordcount.sh
+++ tests/test_wordcount.sh
@@ -7,6 +7,7 @@ test_Android_title_limit_50() {
 	test_result=0
 	for f in `find files -type f | grep title.txt`; do
         count=`tr -d "\n\r" < $f| wc -m`
+	echo "$f is" $count "characters long"
         if [[ $count -gt 50 ]]; then
             echo "$f is" $count "characters long"
             test_result=1
