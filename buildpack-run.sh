6ed7c45a20209d819014e1eea48e6d966b3bed31
sunmiko <30559933+sunmiko@users.noreply.github.com>
2018-09-25 22:47:58 -0700

    Update buildpack-run.sh

--- buildpack-run.sh
+++ buildpack-run.sh
@@ -8,4 +8,17 @@ if [ -d "shunit2" ]; then rm -rf shunit2; fi
 git clone https://svc-l10n:Globalization_123456@github.com/Localization/shunit2.git
 pwd
 ls
+
+### find latest commit hash
+hash=$(echo $(git log --no-merges --pretty=format:'%h' -n 1 HEAD))
+echo
+echo "Commit: $hash"
+
+#find latest changed files
+IFS=$'\n';
+changed=$(git diff-tree --no-commit-id --name-only -r $hash)
+echo "Files:"
+echo $changed
+echo
+
 shunit2/shunit2-2.1.6/src/shunit2 shunit2/tests/test_walkthrough_helptray.sh
